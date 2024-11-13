9eaa2ab22630fcdb2ffbf3dcb3449952425da554
devsohelrana <dev.sohelrana@gmail.com>
2024-11-14 02:21:04 +0600

    Soft Finding Web Portal Hero Section Design

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+// Hero Section Stats Counter Animation
+let animatedNums = document.querySelectorAll(".num");
+let interval = 5000;
+
+animatedNums.forEach((animatedNum) => {
+    let startValue = 0;
+    let endValue = parseInt(animatedNum.getAttribute("data-val"));
+
+    let duration = Math.floor(interval / endValue);
+
+    let counter = setInterval(() => {
+        startValue += 1;
+        let startPlus = "+";
+        animatedNum.textContent = startValue + "" + startPlus;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+
+// Scroll To Top
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
+    scrollProgress.style.background = `conic-gradient(#00a6fb ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+
+
+// INITIALIZE AOS
+AOS.init();
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "lato": ["'Lato', sans-serif"],
+        "exo": ["'Exo 2', sans-serif"],
+      },
+      colors: {
+        "primary": "#EDFEF6",
+        "text": "#111827",
+        "l-green": "#D1FAE5",
+        "l-pink": "#FDE2E4",
+        "l-yellow": "#FEF9C3",
+      },
+    },
+  },
+  plugins: [],
+}
+
